894e1b0860e62e4725c3a1800556ecde331679b8
Corey Gilmore (MBP-Windows) <coreymgilmore@gmail.com>
2015-08-03 20:30:53 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pwds
 Simple Golang Library for Creating and Verifying Bcrypt Passwords
+
+The intended use of this library is to make using golang.org/x/crypto/bcrypt easier.  This library just cleans up usage of the bcrypt library.
+
+---
+
+### Create(input string)
+Creates a hash of a given string input (password) using bcrypt.  Returns the hash as a string.
+
+### Verify(rawPassword string, hashedPassword string)
+Compares a plain text password against a hash.  Returns True (boolean) if the given inputs match.
---
+++ pwds.go
@@ -0,0 +1,46 @@
+/*
+Package pwds is used to create and validate bcrypt passwords.
+This is just a wrapper aroung golang.org/x/crypto/bcrypt that makes it easier to use.
+*/
+
+package pwds
+
+import (
+	"errors"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const BCRYPT_COST = 12
+
+var ErrInvalidPassword = errors.New("invalidPassword")
+
+//*********************************************************************************************************************************
+//FUNCS
+
+//CREATE A PASSWORD
+//in: input (plain text password to hash)
+//out: hashed password as a string
+func Create(input string) string {
+	raw := 		[]byte(input)
+	hash, _ := 	bcrypt.GenerateFromPassword(raw, BCRYPT_COST)
+
+	return string(hash)
+}
+
+//VALIDATE A PASSWORD
+//in: rawPassword (plain text password), hash (hashed password from db)
+//out: boolean, error if password is not valid
+func Verify(rawPassword string, hashedPassword string) (bool, error) {
+	rawPasswordByte := 	[]byte(rawPassword)
+	hashPasswordByte := []byte(hashedPassword)
+
+	err := bcrypt.CompareHashAndPassword(hashPasswordByte, rawPasswordByte)
+	if err != nil {
+		return false, ErrInvalidPassword
+	}
+
+	//password verified
+	return true, nil
+}
